1ee44cb9631c4d3dddb5ac32054128fba3f2e51b
katie hoffer <katiehoffer3@gmail.com>
2020-08-03 20:03:48 -0400

    testing app

---
+++ config.py
@@ -0,0 +1,6 @@
+# GWSIS database connection info
+remote_db_endpoint = 'codingbootcamp.cze9dquabwlh.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_name = 'gwsis'
+remote_db_user = 'root'
+remote_db_pwd = 'Dartlings!'
